465921d1e4102f2c18ec4593ea558cb66949c309
Roman Lysyk <r.lysyk@synchronic.info>
2024-08-04 10:47:16 +0300

    Fix filter by car

--- src/components/Home/Filter/FilterByCar/index.tsx
+++ src/components/Home/Filter/FilterByCar/index.tsx
@@ -15,10 +15,14 @@ interface FilterByCarProps {
 export const FilterByCarComponents: FC<FilterByCarProps> = ({ filters }) => {
 	const t = useAppTranslation();
 
+	const onChange = (name: string, value: number | undefined) => {
+		console.log(name, value)
+	}
+
 	return <>
 		<div className='grid grid-cols-1 md:grid-cols-2 gap-2.5 md:mt-7'>
 			{filters.map(item => {
-				return <MySelect key={item.name} name={item.name} label={item.label} options={item.options}/>
+				return <MySelect key={item.name} name={item.name} label={item.label} options={item.options} onChange={onChange} />
 			})}
 		</div>
 		<div className='mt-4 md:mt-10 flex gap-4 flex-col md:flex-row'>

6ea99e7d421b1bbcd4bee5d2034d5d9d04e2d1bc
Roman Lysyk <r.lysyk@synchronic.info>
2024-09-20 09:46:04 +0300

    Added text file

--- src/components/Product/CreateComment/index.tsx
+++ src/components/Product/CreateComment/index.tsx
@@ -1,7 +1,7 @@
 import { Dispatch, FC, SetStateAction } from 'react';
 import { Controller, useFormContext } from 'react-hook-form'
 
-import { Rating } from '../../Lib';
+import { Rating, TextFile } from '../../Lib';
 import { Language } from '../../../models/language';
 
 interface CreateCommentProps {
@@ -21,47 +21,12 @@ export const CreateCommentComponent: FC<CreateCommentProps> = ({ lang, rate, set
 			<Controller
 				name="name"
 				control={control}
-				render={({ field }) => (
-					<div>
-						<div className="relative mt-4 h-14 w-full min-w-[200px]">
-							<input
-								{...field}
-								className="peer h-full w-full rounded-md border border-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-								placeholder=' '
-							/>
-							<label
-								className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[5.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-blue-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-blue-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-blue-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
-								{lang === Language.UA ? 'Ім\'я*' : 'Имя*'}
-							</label>
-						</div>
-						<p className='ml-2 h-3 text-sm text-red-500 font-semibold'>
-							{ typeof errors?.['name']?.message === 'string' ? errors['name'].message : null }
-						</p>
-					</div>
-				)}
+				render={({ field }) => <TextFile field={ field } label={ lang === Language.UA ? 'Ім\'я*' : 'Имя*' } error={ typeof errors?.['name']?.message === 'string' ? errors['name'].message : null } /> }
 			/>
 			<Controller
 				name="text"
 				control={control}
-				render={({field}) => (
-					<div>
-						<div className="relative mt-4 w-full min-w-[200px]">
-						<textarea
-							rows={4}
-							{...field}
-							className="peer h-full w-full rounded-md border border-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-							placeholder=' '
-						/>
-							<label
-								className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[5.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-blue-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-blue-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-blue-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
-								{lang === Language.UA ? 'Коментар*' : 'Комментарий*'}
-							</label>
-						</div>
-						<p className='ml-2 h-3 text-sm text-red-500 font-semibold'>
-							{typeof errors?.['text']?.message === 'string' ? errors['text'].message : null}
-						</p>
-					</div>
-				)}
+				render={({field}) => <TextFile field={ field } label={ lang === Language.UA ? 'Коментар*' : 'Комментарий*' } error={ typeof errors?.['text']?.message === 'string' ? errors['text'].message : null } isTextarea={ true } /> }
 			/>
 			<div className='mt-3 mb-6 flex items-center'>
 				<span className='mr-2 ml-2 text-sm font-semibold'>

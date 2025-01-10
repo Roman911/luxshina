2c0ace9f29990424204a9d4c8107b55d3cdcc75a
romanlysyk <roma-lysyk@ukr.net>
2025-01-10 14:20:19 +0200

    updated static page

--- src/containers/Alias/index.tsx
+++ src/containers/Alias/index.tsx
@@ -5,13 +5,11 @@ import { useAppSelector } from '../../hooks';
 import { LayoutStaticPage } from './Layout';
 
 export const Alias = () => {
-	const { data: aliasAll } = baseDataAPI.useFetchStatiAliasAllQuery('');
 	const location = useLocation();
 	const pathname = location.pathname.split('/');
 	const slug = pathname[pathname.length - 1];
-	const aliasItem = aliasAll?.header.find(item => item.slug === slug);
 	const { lang } = useAppSelector(state => state.langReducer);
-	const { data, isLoading } = baseDataAPI.useFetchStatiAliasQuery( aliasItem ? `${aliasItem.article_id}` : '1');
+	const { data, isLoading } = baseDataAPI.useFetchStatiAliasQuery(slug);
 
 	if(data?.[slug]) {
 		return <LayoutStaticPage title={ data ? data?.[slug].description[lang].title : '' } data={ data?.[slug] } isLoading={ isLoading } lang={ lang } />
